b61d69ab9396ce216704a50ca6fd0fcb5078797b
Chris Beckman <atadsp@gmail.com>
2016-08-24 14:12:18 -0400

    more index.js tweaks

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function(){
+app.listen(port, function(){
 	console.log('listening');
 });
