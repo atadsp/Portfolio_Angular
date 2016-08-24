e3ed07e7cfe0d02dc82ed47267bfe2689a071243
Chris Beckman <atadsp@gmail.com>
2016-08-24 14:45:30 -0400

    new fix attempt

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var http	= require('http').Server(app);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/public/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(process.env.PORT || 3000, function(){
