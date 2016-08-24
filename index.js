d95441d194e3236be086ef57f13e2e3882470a58
Chris Beckman <atadsp@gmail.com>
2016-08-24 14:23:27 -0400

    updated index, proc, and package

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-var port = process.env.PORT || 3000;
+// var port = process.env.PORT || 3000;
+
+// app.use(express.static(__dirname + '/public'));
+
+// app.listen(port, function(){
+// 	console.log('listening');
+// });
+
+
+var express = require('express');
+var path = require('path');
+var port = process.env.PORT || 8080;
+var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, function(){
-	console.log('listening');
+app.get('*', (req, res)=> {
+	res.sendFile(path.resolve(__dirname, 'index.html'));
 });
+
+app.listen(port);
+console.log("Server Started");
