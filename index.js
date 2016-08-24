d11968b58749fc20637c14321d27d8513922e8a9
Chris Beckman <atadsp@gmail.com>
2016-08-24 15:08:56 -0400

    new strats

--- index.js
+++ index.js
@@ -15,12 +15,38 @@ var app		= express();
 var http	= require('http').Server(app);
 
 //Tell node where to find static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(process.env.PORT || 3000, function(){
+http.listen(process.env.PORT || port, function(){
 	console.log('listening on *:3000');
 });
+
+
+
+
+
+// var express = require('express');
+
+// var app = express();
+
+// var port = 5000
+
+// app.use(express.static('public'));
+// app.use(express.static('src/views'));
+
+// app.get('/', function(req, res){
+//     res.send("hello world");
+// });
+
+
+// app.get('/books', function(req, res){
+//     res.send("hello books");
+// });
+
+// app.listen(process.env.PORT || port, function(err){
+//     console.log('running server on port ' + port);
+// });
